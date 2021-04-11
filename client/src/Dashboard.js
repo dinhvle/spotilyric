a259116ab41fefdf8f3256f6832c034ad7426c60
Dinh Le <dinh.v.le@gmail.com>
2021-04-11 00:33:35 -0500

    :sparkles: implement search bar for tracks data from spotify

--- client/src/Dashboard.js
+++ client/src/Dashboard.js
@@ -1,11 +1,67 @@
 import useAuth from "./useAuth";
+import { Container, Form } from "react-bootstrap";
+import { useEffect, useState } from "react";
+import SpotifyWebApi from "spotify-web-api-node";
+
+const spotifyApi = new SpotifyWebApi({
+  clientId: "336cc5f653c948679d20e75dd48b99d3",
+});
 
 export default function Dashboard({ code }) {
   const accessToken = useAuth(code);
+  const [search, setSearch] = useState("");
+  const [searchResults, setSearchResults] = useState([]);
+  console.log(searchResults);
+
+  useEffect(() => {
+    if (!accessToken) return;
+    spotifyApi.setAccessToken(accessToken);
+  }, [accessToken]);
+
+  useEffect(() => {
+    if (!search) return setSearchResults([]);
+    if (!accessToken) return;
+
+    let cancel = false;
+
+    spotifyApi.searchTracks(search).then((res) => {
+      if (cancel) return;
+
+      setSearchResults(
+        res.body.tracks.items.map((track) => {
+          const smallestAlbumImage = track.album.images.reduce(
+            (smallest, image) => {
+              if (image.height < smallest.height) return image;
+              return smallest;
+            },
+            track.album.images[0]
+          );
+
+          return {
+            albumUrl: smallestAlbumImage.url,
+            artist: track.artists[0].name,
+            title: track.name,
+            uri: track.uri,
+          };
+        })
+      );
+    });
+
+    return () => (cancel = true);
+  }, [search, accessToken]);
+
   return (
-    <div>
-      <p>AccessToken: {accessToken}</p>
-      <p>Code: {code}</p>
-    </div>
+    <Container className="d-flex flex-column py-2" style={{ height: "100vh" }}>
+      <Form.Control
+        type="search"
+        placeholder="Search Songs/Artists"
+        value={search}
+        onChange={(e) => setSearch(e.target.value)}
+      />
+      <div className="flex-grow-1 my-2" style={{ overflowY: "auto" }}>
+        Songs
+      </div>
+      <div>Bottom</div>
+    </Container>
   );
 }
